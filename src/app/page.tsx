bad8ec65684bae28a2aa6c97002231950922930d
edilan-ribeiro <eddyoneal2@hotmail.com>
2023-12-21 22:18:56 -0300

    Page update
    - added form provider to get the form data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,39 @@
 'use client'
 
-import styled from "styled-components";
-import { Card } from "@/components/Card/Card";
-import { Form } from "@/components/Form/Form";
-
+import styled from 'styled-components'
+import { Card } from '@/components/Card/Card'
+import { Form, createCardSchema } from '@/components/Form/Form'
+import { FormProvider, UseFormReturn, useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Thanks } from '@/components/Thanks/Thanks'
+import { useState } from 'react'
 
 export default function Home() {
-  return (
-    <Main>
-      <Card/>
-      <Form/>
-    </Main>
-  )
-}
+	const methods: UseFormReturn<{
+		name: string
+		cardn: number
+		month: number
+		year: number
+		cvc: number
+	}> = useForm<createCardSchema>({
+		resolver: zodResolver(createCardSchema),
+	})
 
+	const [formSent, setFormSent] = useState<boolean>(false)
+
+	return (
+		<Main>
+			<FormProvider {...methods}>
+				<Card />
+				{formSent === false ? (
+					<Form sent={setFormSent} />
+				) : (
+					<Thanks resetpage={setFormSent} />
+				)}
+			</FormProvider>
+		</Main>
+	)
+}
 
 const Main = styled.main`
     min-width: 100vw;
