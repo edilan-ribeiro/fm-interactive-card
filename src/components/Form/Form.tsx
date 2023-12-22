b4f5d67cae31504154b9d40bf0e87720cadb9a92
edilan-ribeiro <eddyoneal2@hotmail.com>
2023-12-22 18:27:46 -0300

    Component update
    -changed validation on card number due to format
    - updated prop name
    - updated function name

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,4 +1,4 @@
-import { useFormContext } from 'react-hook-form'
+import { Controller, useFormContext } from 'react-hook-form'
 import { z } from 'zod'
 import {
 	ConfirmButton,
@@ -12,18 +12,22 @@ import {
 	Labels,
 	SecurityFields,
 } from './Form.Styles'
-import { Dispatch, SetStateAction } from 'react'
+import React, { Dispatch, SetStateAction } from 'react'
+import { MaskedInput } from './FormStyles/MaskedInput'
 
 export type createCardSchema = z.infer<typeof createCardSchema>
 
 export const createCardSchema = z.object({
-	name: z.string().min(1, "Can't be blank").min(5, 'Name too small'),
-
-	cardn: z.coerce
-		.number({ invalid_type_error: 'Wrong format, numbers only' })
+	name: z
+		.string()
 		.min(1, "Can't be blank")
+		.min(5, 'Name too small')
+		.refine((name) => name.match(/^[A-Za-z\s]*$/), 'Invalid name'),
+
+	cardn: z
+		.string({ required_error: "Can't be blank" })
 		.refine(
-			(cardNumber) => cardNumber.toString().length === 16,
+			(cardNumber) => cardNumber.replace(/\s+/g, '').length === 16,
 			'Must have 16 digits'
 		),
 
@@ -43,40 +47,46 @@ export const createCardSchema = z.object({
 		.min(1, "Can't be blank"),
 })
 
-type formSent = {
-	sent: Dispatch<SetStateAction<boolean>>
+type datasent = {
+	datasent: Dispatch<SetStateAction<boolean>>
 }
 
-export const Form = ( { sent } : formSent ) => {
+export const Form = ({ datasent }: datasent) => {
 	//prettier-ignore
-	const { register ,handleSubmit , formState: { errors } } = useFormContext<createCardSchema>()
+	const { register, handleSubmit, control, formState: { errors } } = useFormContext<createCardSchema>()
 
-	function dataSent() {
-		sent(true)
+	function formSent() {
+		datasent(true)
 	}
 
-
 	return (
-		<FormSide onSubmit={handleSubmit(dataSent)}>
+		<FormSide onSubmit={handleSubmit(formSent)}>
 			<InputContainer>
 				<Labels htmlFor="name">cardholder name</Labels>
 				<InputField
 					id="name"
 					placeholder="e.g. Jane Appleseed"
 					maxLength={20}
-					errors={errors}
+					$errors={errors}
 					{...register('name')}
 				/>
 				{errors.name && <InputErrors>{errors.name.message}</InputErrors>}
 			</InputContainer>
 			<InputContainer>
 				<Labels htmlFor="cardn">card number</Labels>
-				<InputField
-					id="cardn"
-					placeholder="e.g 1234 5678 9123 000"
-					maxLength={16}
-					errors={errors}
-					{...register('cardn')}
+				<Controller
+					name="cardn"
+					control={control}
+					render={({ field }) => (
+						<MaskedInput
+							id="cardn"
+							errors={errors}
+							placeholder="e.g 1234 5678 9123 000"
+							format="#### #### #### ####"
+							value={field.value}
+							onValueChange={(values) => field.onChange(values.value)}
+						/>
+					)}
 				/>
 				{errors.cardn && <InputErrors>{errors.cardn.message}</InputErrors>}
 			</InputContainer>
@@ -89,22 +99,26 @@ export const Form = ( { sent } : formSent ) => {
 								id="month"
 								placeholder="MM"
 								maxLength={2}
-								errors={errors}
+								$errors={errors}
 								{...register('month')}
 							/>
-							{errors.month && <InputErrors>{errors.month.message}</InputErrors> || errors.year && <InputErrors>{errors.year.message}</InputErrors>}
+							{(errors.month && (
+								<InputErrors>{errors.month.message}</InputErrors>
+							)) ||
+								(errors.year && (
+									<InputErrors>{errors.year.message}</InputErrors>
+								))}
 						</div>
 						<div>
 							<InputField
 								id="year"
 								placeholder="YY"
 								maxLength={2}
-								errors={errors}
+								$errors={errors}
 								{...register('year')}
 							/>
 						</div>
 					</DateContainer>
-
 				</ExpireField>
 
 				<CvcField>
@@ -113,7 +127,7 @@ export const Form = ( { sent } : formSent ) => {
 						id="cvc"
 						placeholder="e.g. 123"
 						maxLength={3}
-						errors={errors}
+						$errors={errors}
 						{...register('cvc')}
 					/>
 					{errors.cvc && <InputErrors>{errors.cvc.message}</InputErrors>}
